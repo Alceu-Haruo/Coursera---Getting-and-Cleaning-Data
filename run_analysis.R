72486254c588d5fdfb77abc3c61b93b63d567b9c
Alceu-Haruo <alceu.haruo@uol.com.br>
2022-03-23 12:31:38 -0300

    Versionf of 2022-03-23

--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,25 @@
-##Coursera assingment for the course "Obtaining and Cleaning Data"
+##Coursera assingment for the course "Getting Cleaning Data"
 
-##directory of assignment
-setwd("D:/Coursera/Obtaining and cleaning data/Assignment/UCI HAR Dataset")
+##loading library
 library(dplyr)
 
+##set working directory
+setwd("D:/Coursera/Getting_and_cleaning_Data")
+
+##download and unzip the data
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(url, "Dataset.zip")
+unzip("Dataset.zip")
+
 ##loading files
-features <- read.table("features.txt", col.names = c("id_function", "functions"))
-activities <- read.table("activity_labels.txt", col.names = c("id_activ","activity"))
-subject_test <- read.table("test/subject_test.txt", col.names = "subject")
-x_test <- read.table("test/X_test.txt", col.names = features$functions)
-y_test <- read.table("test/y_test.txt", col.names = "code")
-subject_train <- read.table("train/subject_train.txt", col.names = "subject")
-x_train <- read.table("train/X_train.txt", col.names = features$functions)
-y_train <- read.table("train/y_train.txt", col.names = "code")
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("id_function", "functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("id_activ","activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
 
 ##Exercise 1: Merges the training and the test sets to create one data set
 merge_subject <- rbind(subject_test,subject_train)
